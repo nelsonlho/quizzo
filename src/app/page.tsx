a8456da8d98de019de6b8261ea344734d1235cf1
nelsonlho <nelsonlho@gmail.com>
2023-07-31 12:04:04 -0700

    set up theme toggle

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import Image from 'next/image';
 import { Button } from '@/components/ui/button';
+import { Prisma } from '@prisma/client';
 
 export default function Home() {
   return <Button>Hello world</Button>;
---
+++ src/components/SignInButton.tsx
@@ -0,0 +1,22 @@
+'use client';
+
+import { signIn } from 'next-auth/react';
+import { Button } from './ui/button';
+
+type Props = {
+  text: string;
+};
+
+const SignInButton = ({ text }: Props) => {
+  return (
+    <Button
+      onClick={() => {
+        signIn('google').catch(console.error);
+      }}
+    >
+      {text}
+    </Button>
+  );
+};
+
+export default SignInButton;
