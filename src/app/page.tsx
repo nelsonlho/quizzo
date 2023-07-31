9240c83c16a9ad97298171af81c7642a52cb8878
nelsonlho <nelsonlho@gmail.com>
2023-07-31 16:22:42 -0700

    set up card, dashboard

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,36 @@
-import Image from 'next/image';
+import SignInButton from '@/components/SignInButton';
 import { Button } from '@/components/ui/button';
-import { Prisma } from '@prisma/client';
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card';
+import { getAuthSession } from '@/lib/nextauth';
+import { Session } from '';
+import { redirect } from 'next/navigation';
 
-export default function Home() {
-  return <Button>Hello world</Button>;
+export default async function Home() {
+  const session = await getAuthSession();
+  if (session?.user) {
+    return redirect('/dashboard');
+  }
+  return (
+    <div className="flex items-center justify-center h-screen">
+      <div className="bg-white p-6 rounded shadow-md mx-auto my-auto">
+        <Card className="w-[300px]">
+          <CardHeader>
+            <CardTitle>Welcome to Quizzo</CardTitle>
+            <CardDescription>
+              AI generated questions with topics of your choice
+            </CardDescription>
+          </CardHeader>
+          <CardContent>
+            <SignInButton text="Sign In with Google"></SignInButton>
+          </CardContent>
+        </Card>
+      </div>
+    </div>
+  );
 }
