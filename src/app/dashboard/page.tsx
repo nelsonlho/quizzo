ebc227fa3f2c197d5f84ab405b3efd76c2ba6c89
nelsonlho <nelsonlho@gmail.com>
2023-07-31 16:39:58 -0700

    scaffolded dashboard components

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,9 +1,33 @@
-import React from 'react';
+import { getAuthSession } from '@/lib/nextauth';
+import { redirect } from 'next/navigation';
+import QuizMeCard from '@/components/dashboard/QuizMeCard';
+import HistoryCard from '@/components/dashboard/HistoryCard';
+import HotTopicsCard from '@/components/dashboard/HotTopicsCard';
+import RecentActivityCard from '@/components/dashboard/RecentActivities';
 
 type Props = {};
 
-const Dashboard = (props: Props) => {
-  return <div> Dashboard</div>;
+const Dashboard = async (props: Props) => {
+  const session = await getAuthSession();
+  if (!session?.user) {
+    return redirect('/');
+  }
+  return (
+    <main className="p-8 mx-auto max-w-7xl">
+      <div className="flex items-center">
+        <h2 className="m4-2 text-3xl font-bold tracking-tight">Dashboard</h2>
+      </div>
+
+      <div className="grid gap-4 mt-4 md:grid-cols-2">
+        <QuizMeCard />
+        <HistoryCard />
+      </div>
+      <div className="grid gap-4 mt-4 md:grid-cols-2 lg:grid-cols-7">
+        <HotTopicsCard />
+        <RecentActivityCard />
+      </div>
+    </main>
+  );
 };
 
 export default Dashboard;
